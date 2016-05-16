8fd954a40dc4d3075f32220c01d7a37653ffa02a
Robert Young <apple@Robs-MacBook-Pro.local>
2016-05-16 22:22:21 +0100

    Updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,14 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
   };
 
   if(process.env.TRAVIS){
